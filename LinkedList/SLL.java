ec34ced8365d1232303eb761e704c584390d41a5
Julius-tech <jbolaifa@gmail.com>
2020-11-25 10:41:23 -0600

    Singly linked list constructor

--- LinkedList/SLL.java
+++ LinkedList/SLL.java
@@ -1,9 +1,25 @@
 public class SLL{
-
-  private class Node{
+    public static void main(String[] args) {
+      
+      SLL list = new SLL();
+    }
     
-  }
-  public static void main(String[] args){
-
-  }
+    private class Node{
+        private int data;
+        private Node next;
+        
+        private Node(int data, Node next){
+            
+            this.data = data;
+            this.next = next;
+        }
+    }
+    
+    private Node head;
+    private int size;
+    
+    public SLL(){
+      this.head = new Node(0, null);
+      this.size = 0;
+    }
 }
